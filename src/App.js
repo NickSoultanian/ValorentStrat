9af35c427c6d0021c1d7fa2ad0948f32183f085e
Nicholas Soultanian <nicholas.soultanian@gmail.com>
2020-07-28 16:06:10 -0700

    pls

--- src/App.js
+++ src/App.js
@@ -6,18 +6,29 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Alright so Hopefully we have something good going on here.
-        </p>
-        <a
+        <h1>
+          Valorant Strats
+        </h1>
+        <div>
+
+        </div>
+        <h6>
+          A strategy tool to help you and your team destroy any opponent.
+        </h6>
+      </header>
+      <body className = "App-body">
+      <p>
+        Alright so Hopefully we have something good going on here.
+      </p>
+      <a
           //className="App-link"
           //href="https://reactjs.org"
           //target="_blank"
           //rel="noopener noreferrer"
-        >
-          Get ready to Git Guud
-        </a>
-      </header>
+      >
+        Get ready to Git Guud
+      </a>
+      </body>
     </div>
   );
 }
