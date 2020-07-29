038332adc8d80a3e65f98ccd2fe72e9f0576280b
Nicholas Soultanian <nicholas.soultanian@gmail.com>
2020-07-29 09:27:37 -0700

    kinda embarssing

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 // See ../copy-to-example.sh
-
+import './MapContainer.css';
 import { MapInteractionCSS } from 'react-map-interaction';
+import { slide as Menu } from 'react-burger-menu'
 
 class MapContainer extends Component {
     constructor(props) {
@@ -14,20 +15,27 @@ class MapContainer extends Component {
 
     render() {
         // set container node at an origin other than client 0,0 to make sure we handle this case
-        const offset = 20;
 
+        const offset = 50;
         const style = {
-            position: 'absolute',
+            position: 'relative',
+            float: 'right',
             top: offset,
             right: offset,
             width: `calc(50vw - ${offset}px)`,
-            height: `calc(50vh - ${offset}px)`,
-            border: '1px solid blue'
+            height: `calc(70vh - ${offset}px)`,
+            border: '5px Solid Black'
         }
 
         const { scale, translation } = this.state;
         return (
             <div style={style}>
+                <Menu>
+                    <a id="home" className="menu-item" href="/">Home</a>
+                    <a id="about" className="menu-item" href="/about">About</a>
+                    <a id="contact" className="menu-item" href="/contact">Contact</a>
+                    <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+                </Menu>
                 <MapInteractionCSS
                     
                     scale={scale}
