5d4f3384ca5e054faf0e39f234f5fad41d18642f
Nicholas Soultanian <nicholas.soultanian@gmail.com>
2020-07-30 09:29:18 -0700

    updated dropdown button menu

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -22,6 +22,7 @@ class MapContainer extends Component {
 
         const offset = 50;
         const style = {
+            zIndex: 0,
             position: 'absolute',
             float: 'right',
             top: offset,
@@ -30,35 +31,49 @@ class MapContainer extends Component {
             height: `calc(70vh - ${offset}px)`,
             border: '5px Solid Black'
         }
+        const style2 = {
+            zIndex: 1,
+            position: 'absolute',
+            float: 'right',
+            top: offset + 10,
+            right: offset + 830,
+            //width: `calc(50vw - ${offset}px)`,
+            //height: `calc(70vh - ${offset}px)`,
+            //border: '5px Solid Black'
+        }
 
         const { scale, translation } = this.state;
         return (
-            <div style={style}>
-                <ButtonGroup vertical>
-                    top: 0
-                <DropdownButton as={ButtonGroup} title="Dropdown" id="bg-vertical-dropdown-2">
-
-                    <Button>
-                        Button
-                    </Button>
-                    <Button>Button</Button>
-                </DropdownButton>
-                </ButtonGroup>
-                <MapInteractionCSS
-                    scale={scale}
-                    translation={translation}
-                    onChange={({ scale, translation }) => this.setState({ scale, translation })}
-                    defaultScale={1}
-                    defaultTranslation={{ x: 0, y: 0 }}
-                    minScale={0.05}
-                    maxScale={5}
-                    showControls
-                >
-                    <div style={{ position: 'relative' }}>
+            <div>
+                <div style={ style2 }>
+                    <ButtonGroup vertical>
+                        <DropdownButton as={ButtonGroup} title="Dropdown" id="bg-vertical-dropdown-2">
+                            <Button>
+                                Button
+                            </Button>
+                            <Button>
+                                Button
+                            </Button>
+                        </DropdownButton>
+                    </ButtonGroup>
+                </div>
+                    <div style={style}>
+                    <MapInteractionCSS
+                        scale={scale}
+                        translation={translation}
+                        onChange={({ scale, translation }) => this.setState({ scale, translation })}
+                        defaultScale={1}
+                        defaultTranslation={{ x: 0, y: 0 }}
+                        minScale={0.05}
+                        maxScale={5}
+                        showControls
+                    >
+                        <div style={{ position: 'relative' }}>
                       
-                        <img src= {require('./assets/splitNoLabels.png')} alt={"this is the map"}/>
-                    </div>
-                </MapInteractionCSS>
+                            <img src= {require('./assets/splitNoLabels.png')} alt={"this is the map"}/>
+                        </div>
+                    </MapInteractionCSS>
+                </div>
             </div>
         );
     }
